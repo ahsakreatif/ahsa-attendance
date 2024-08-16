2921760c812267f78a4d720ff59b10913e97afcf
TedoHarisC <tedoharischandra29@gmail.com>
2024-08-16 14:18:41 +0700

    Membuat Maps di Presensi Page

--- resources/views/livewire/presensi.blade.php
+++ resources/views/livewire/presensi.blade.php
@@ -1,3 +1,30 @@
 <div>
-    <h1>Dari livewire Presensi</h1>
+    <div class="container mx-auto">
+        <div class="bg-white p-6 rounded-lg mt-3 shadow-lg">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+                <div>
+                    <h2 class="text-2xl font-bold mb-2">Informasi Pegawai</h2>
+                    <div class="bg-gray-100 p-4 rounded-lg">
+                        <p><strong>Nama Pegawai : </strong> {{Auth::user()->name}}</p>
+                        <p><strong>Kantor : </strong> Main Office</p>
+                        <p><strong>Shift : </strong> Shift Pagi</p>
+                        <p><strong></strong></p>
+                    </div>
+                </div>
+                <div>
+                    <h2 class="text-2xl font-bold mb-2">Presensi</h2>
+                    <div id="map" class="mb-4 rounded-lg border border-gray-300 w-auto h-[400px]"></div>
+                    <button type="button" class="px-4 py-2 bg-blue-500 text-white rounded">Tag Location</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <script>
+        const map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    </script>
 </div>
